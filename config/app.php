380606d64dbf2727465f55bfa4a2948c84807d70
bappy25 <bappychanting@outlook.com>
2018-07-13 01:53:23 +0600

    added PDF

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*== List of countries == */
         'Webpatser\Countries\CountriesServiceProvider',
