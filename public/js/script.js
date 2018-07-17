b1625bcc08df19998e2bf721a319200180d8aba2
bappy25 <bappychanting@outlook.com>
2018-07-18 00:43:39 +0600

    working with pagination table

--- public/js/script.js
+++ public/js/script.js
@@ -10,24 +10,27 @@ $(document).ready(function(){
   footerAlign();
   setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 50);
 
-   $('.delete_sweet_alert').on('click', function () {
-       swal({   
-        title: "Are you sure?",   
-        text: "You will not be able to recover this imaginary file!",   
-        type: "warning",   
-        showCancelButton: true,   
-        confirmButtonColor: "#DD6B55",   
-        confirmButtonText: "Yes, delete it!",   
-        cancelButtonText: "No, cancel plx!",   
-        closeOnConfirm: false,   
-        closeOnCancel: false }, 
-        function(isConfirm){   
-           if (isConfirm) {     
-               swal("Deleted!", "Your imaginary file has been deleted.", "success");   } 
-           else {
-               swal("Cancelled", "Your imaginary file is safe :)", "error");   } 
-        });
-    });
+  $('#dtBasicExample').DataTable();
+  $('.dataTables_length').addClass('bs-select');
+
+  $('.delete_sweet_alert').on('click', function () {
+     swal({   
+      title: "Are you sure?",   
+      text: "You will not be able to recover this imaginary file!",   
+      type: "warning",   
+      showCancelButton: true,   
+      confirmButtonColor: "#DD6B55",   
+      confirmButtonText: "Yes, delete it!",   
+      cancelButtonText: "No, cancel plx!",   
+      closeOnConfirm: false,   
+      closeOnCancel: false }, 
+      function(isConfirm){   
+         if (isConfirm) {     
+             swal("Deleted!", "Your imaginary file has been deleted.", "success");   } 
+         else {
+             swal("Cancelled", "Your imaginary file is safe :)", "error");   } 
+      });
+  });
 
    $("#scrollToTopButton").hide();
   
