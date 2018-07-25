409147dc8e8dbfb777b783def733e68ea2c0f70e
bappy25 <bappychanting@outlook.com>
2018-07-25 17:16:44 +0600

    some changes

--- resources/views/profile/messages/offer.blade.php
+++ resources/views/profile/messages/offer.blade.php
@@ -7,25 +7,24 @@
 <!-- Page Content -->
     <div class="container">
 
-      <!-- Page Heading/Breadcrumbs -->
-      <h1 class="mt-4 mb-3">
-        User Content
-      </h1>
+		<!-- Page Heading/Breadcrumbs -->
+		<h1 class="mt-4 mb-3">
+		User Content
+		</h1>
 
-      <div class="bc-icons">
-          <ol class="breadcrumb blue-gradient">
-              <li class="breadcrumb-item"><a class="white-text" href="{{ route('buyandtravel') }}">Home</a></li>
-              <li class="breadcrumb-item"><i class="fa fa-hand-o-right mx-2 white-text" aria-hidden="true"></i>User Content</li>
-              <li class="breadcrumb-item"><a class="white-text" href="{{ route('messages.all') }}"><i class="fa fa-hand-o-right mx-2 white-text" aria-hidden="true"></i>Messages</a></li>
-              <li class="breadcrumb-item active"><i class="fa fa-hand-o-right mx-2 white-text" aria-hidden="true"></i>Offer Conversation</li>
-          </ol>
-      </div>
+		<div class="bc-icons">
+			<ol class="breadcrumb blue-gradient">
+				<li class="breadcrumb-item"><a class="white-text" href="{{ route('buyandtravel') }}">Home</a></li>
+				<li class="breadcrumb-item"><i class="fa fa-hand-o-right mx-2 white-text" aria-hidden="true"></i>User Content</li>
+				<li class="breadcrumb-item"><a class="white-text" href="{{ route('messages.all') }}"><i class="fa fa-hand-o-right mx-2 white-text" aria-hidden="true"></i>Messages</a></li>
+				<li class="breadcrumb-item active"><i class="fa fa-hand-o-right mx-2 white-text" aria-hidden="true"></i>Offer Conversation</li>
+			</ol>
+		</div>
 
         <!-- Content Row -->
         <div class="row">
-
-          <!-- Sidebar Column -->
-          @include('profile.sidebar')
+          	<!-- Sidebar Column -->
+          	@include('profile.sidebar')
 
             <!-- Content Column -->
             <div class="col-lg-10 mb-4">
@@ -33,7 +32,6 @@
             </div> 
            
           </div>
-        </div>
         <!-- /.row -->
 
     </div>
