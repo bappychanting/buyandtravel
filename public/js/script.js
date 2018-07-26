79079db21cbab62bb0cb26398de12359a18c7b6a
bappy25 <bappychanting@outlook.com>
2018-07-27 04:40:56 +0600

    added comments to script

--- public/js/script.js
+++ public/js/script.js
@@ -1,180 +1,203 @@
+/*==================== start of windows resize functions  ====================*/
+
 $( window ).resize(function() {
+
+    // Align footer on windows resize
+
   footerAlign();
+
 });
+
+/*==================== end of windows resize functions  ====================*/
+
+/*==================== start of document ready functions  ====================*/
 
 $(document).ready(function(){
 
   time = 0;
 
+    // Set all html selects as material select
+
   $('.mdb-select').material_select();
+
+    // Align footer
+
   footerAlign();
+
+    // Set when the loading screen will stop
+
   setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 50);
 
-  $('.delete_sweet_alert').on('click', function () {
-     swal({   
-      title: "Are you sure?",   
-      text: "You will not be able to recover this imaginary file!",   
-      type: "warning",   
-      showCancelButton: true,   
-      confirmButtonColor: "#DD6B55",   
-      confirmButtonText: "Yes, delete it!",   
-      cancelButtonText: "No, cancel plx!",   
-      closeOnConfirm: false,   
-      closeOnCancel: false }, 
-      function(isConfirm){   
-         if (isConfirm) {     
-             swal("Deleted!", "Your imaginary file has been deleted.", "success");   } 
-         else {
-             swal("Cancelled", "Your imaginary file is safe :)", "error");   } 
+    // hide button to scroll to top  
+
+  $("#scrollToTopButton").hide();
+  
+    //  Check to see if the window is top if not then display button
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 200) {
+      $("#scrollToTopButton").fadeIn();
+    } else {
+      $("#scrollToTopButton").fadeOut();
+    }
+  });
+  
+    //  Click event to scroll to top
+
+  $("#scrollToTopButton").click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
+
+    // Showing Notifications
+
+  if ($(".info_messages").length) {
+      $( ".info_messages" ).each(function() {
+        notificationLoop(time, $(this), "#", "info", "top", "right", 20, 120, 'animated fadeInDown', 'animated fadeOutUp');
+        time++;
+      });
+  } 
+  if ($(".success_messages").length) {
+      $( ".success_messages" ).each(function() {
+        notificationLoop(time, $(this), "#", "success", "top", "right", 20, 120, 'animated fadeInDown', 'animated fadeOutUp');
+        time++;
+      });
+  } 
+  if ($(".warning_messages").length) {
+      $( ".warning_messages" ).each(function() {
+        notificationLoop(time, $(this), "#", "warning", "top", "right", 20, 120, 'animated fadeInDown', 'animated fadeOutUp');
+        time++;
+      });
+  } 
+  if ($(".error_messages").length) {
+      $( ".error_messages" ).each(function() {
+        notificationLoop(time, $(this), "#", "danger", "top", "right", 20, 120, 'animated fadeInDown', 'animated fadeOutUp');
+        time++;
+      });
+  } 
+
+    //  Show image preview
+
+  $('.input_image').change(function() {
+      readURL(this, $(this).index());
+  });
+
+    //  Refresh preview on modal close
+
+  $('#updateimage').on('hidden.bs.modal', function (e) {
+    $('input[name=image]').empty().val('');
+    $('.file-path').empty().val('');
+    $('.preview_input').attr('src', 'http://placehold.it/200');
+  });
+
+    // Open and close details on button click
+
+  $('#showDetails').on('show.bs.collapse', function () {
+    $('#showDetailsButton').empty().append("<i class='fa fa-folder fa-sm pr-2'></i>Click Here to Close Details");
+  });
+
+  $('#showDetails').on('hidden.bs.collapse', function () {
+    $('#showDetailsButton').empty().append("<i class='fa fa-folder-open fa-sm pr-2'></i>Click Here to Show Details");
+  });
+
+    //  Jquery form for uploading image and showing progress
+
+  (function() {
+    $('.upload_image').ajaxForm({
+      beforeSend: function() {
+      },
+      uploadProgress: function() {
+        $(".image_modal").empty().append("<div class='modal-body text-center mb-1'><h5 class='mt-1 mb-2'>Uploading Image</h5><div class='progress primary-color-dark'><div class='indeterminate'></div></div></div>");
+      },
+      success: function() {
+        $(".image_modal").empty().append("<div class='modal-body text-center mb-1'><h5 class='mt-1 mb-2 light-green-text'><i class='fa fa-check-circle'></i> Image Uploaded</h5><p class='mt-1 mb-2 deep-orange-text'>Wait till the image is being saved...</p><div class='progress primary-color-dark'><div class='indeterminate'></div></div></div>").fadeIn("slow");        
+      },
+      error: function() {
+       $(".image_modal").empty().append("<div class='modal-body text-center mb-1'><h5 class='mt-1 mb-2 red-text'><i class='fa fa-warning'></i> Image can't be Uploaded!</h5><p class='mt-1 mb-2 light-blue-text'>Something went wrong in the server. Wait till the page refreshes...</p><div class='progress primary-color-dark'><div class='indeterminate'></div></div></div>").fadeIn("slow");        
+      },
+      complete: function(xhr) {
+        location.reload();
+      }
+    }); 
+  })();
+
+    //  Show offer details on modal popup
+
+  $('.view_offer_details_button').click(function() {
+    var offer = $(this).data('offer');
+    var order = $(this).data('order');
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr('content')
+      }
+    });
+    $.ajax({
+      url: "offer/details",
+      type: 'POST',
+      data: {
+        'offer_id': offer,
+        'order_id': order
+      },
+      dataType: 'JSON',
+      success:function(response){
+        $('#modal_offer_id').val(offer);
+        $('#modal_order_id').val(order);
+        $("#modal_accept_offer_btn").prop("disabled",false);
+        $("#modal_offer_details").empty().append('<ul class="list-group list-group-flush mb-3"><li class="list-group-item"><i class="fa fa-user fa-sm pr-2"></i><strong>Added By: </strong>' +response['user']+'</li><li class="list-group-item"><i class="fa fa-cart-plus fa-sm pr-2"></i><strong>Quantity: </strong>'+response['quantity']+'</li><li class="list-group-item"><i class="fa fa-dollar fa-sm pr-2"></i><strong>Asking Price: </strong>'+response['price']+'/=</li><li class="list-group-item"><i class="fa fa-calendar-check-o fa-sm pr-2"></i><strong>Delivery Date: </strong>'+response['date']+'</li></ul>'+response['details']);
+      },
+      error: function(response){
+        $("#modal_accept_offer_btn").prop("disabled",true);
+        $("#modal_offer_details").empty().append('<p class="font-weight-bold text-center">Sorry, no data returned from server...</p>');
+      }
+    });
+  });
+
+    //  Sweet alert for warning
+
+  $('.form_warning_sweet_alert').on('click',function(e){
+      e.preventDefault();
+      var form = $(this).parents('form');
+      var title = $(this).attr('title');
+      var text = $(this).attr('text');
+      var confirmButtonText = $(this).attr('confirmButtonText');
+      swal({
+          title: title,
+          text: text,
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#2196f3",
+          confirmButtonText: confirmButtonText,
+          closeOnConfirm: false
+      }, function(isConfirm){
+          if (isConfirm) form.submit();
       });
   });
 
-   $("#scrollToTopButton").hide();
-  
-      //Check to see if the window is top if not then display button
-    $(window).scroll(function(){
-      if ($(this).scrollTop() > 200) {
-        $("#scrollToTopButton").fadeIn();
-      } else {
-        $("#scrollToTopButton").fadeOut();
-      }
-    });
-    
-      //Click event to scroll to top
-    $("#scrollToTopButton").click(function(){
-      $('html, body').animate({scrollTop : 0},800);
-      return false;
-    });
+      // MSB Jquery Datatable
 
-      // Notifications
-    if ($(".info_messages").length) {
-        $( ".info_messages" ).each(function() {
-          notificationLoop(time, $(this), "#", "info", "top", "right", 20, 120, 'animated fadeInDown', 'animated fadeOutUp');
-          time++;
-        });
-    } 
-    if ($(".success_messages").length) {
-        $( ".success_messages" ).each(function() {
-          notificationLoop(time, $(this), "#", "success", "top", "right", 20, 120, 'animated fadeInDown', 'animated fadeOutUp');
-          time++;
-        });
-    } 
-    if ($(".warning_messages").length) {
-        $( ".warning_messages" ).each(function() {
-          notificationLoop(time, $(this), "#", "warning", "top", "right", 20, 120, 'animated fadeInDown', 'animated fadeOutUp');
-          time++;
-        });
-    } 
-    if ($(".error_messages").length) {
-        $( ".error_messages" ).each(function() {
-          notificationLoop(time, $(this), "#", "danger", "top", "right", 20, 120, 'animated fadeInDown', 'animated fadeOutUp');
-          time++;
-        });
-    } 
+  $('#dataTable').DataTable();
+  $('#dataTable_wrapper').find('label').each(function () {
+    $(this).parent().append($(this).children());
+  });
+  $('#dataTable_wrapper .dataTables_filter').find('input').each(function () {
+    $('input').attr("placeholder", "Search");
+    $('input').removeClass('form-control-sm');
+  });
+  $('#dataTable_wrapper .dataTables_length').addClass('d-flex flex-row');
+  $('#dataTable_wrapper .dataTables_filter').addClass('md-form');
+  $('#dataTable_wrapper select').removeClass('custom-select custom-select-sm form-control form-control-sm');
+  $('#dataTable_wrapper select').addClass('mdb-select');
+  $('#dataTable_wrapper .mdb-select').material_select();
+  $('#dataTable_wrapper .dataTables_filter').find('label').remove();
 
-    $('.input_image').change(function() {
-        readURL(this, $(this).index());
-    });
-
-    $('#showDetails').on('show.bs.collapse', function () {
-      $('#showDetailsButton').empty().append("<i class='fa fa-folder fa-sm pr-2'></i>Click Here to Close Details");
-    })
-
-    $('#showDetails').on('hidden.bs.collapse', function () {
-      $('#showDetailsButton').empty().append("<i class='fa fa-folder-open fa-sm pr-2'></i>Click Here to Show Details");
-    })
-
-    $('#updateimage').on('hidden.bs.modal', function (e) {
-      $('input[name=image]').empty().val('');
-      $('.file-path').empty().val('');
-      $('.preview_input').attr('src', 'http://placehold.it/200');
-    });
-
-    (function() {
-      $('.upload_image').ajaxForm({
-        beforeSend: function() {
-        },
-        uploadProgress: function() {
-          $(".image_modal").empty().append("<div class='modal-body text-center mb-1'><h5 class='mt-1 mb-2'>Uploading Image</h5><div class='progress primary-color-dark'><div class='indeterminate'></div></div></div>");
-        },
-        success: function() {
-          $(".image_modal").empty().append("<div class='modal-body text-center mb-1'><h5 class='mt-1 mb-2 light-green-text'><i class='fa fa-check-circle'></i> Image Uploaded</h5><p class='mt-1 mb-2 deep-orange-text'>Wait till the image is being saved...</p><div class='progress primary-color-dark'><div class='indeterminate'></div></div></div>").fadeIn("slow");        
-        },
-        error: function() {
-         $(".image_modal").empty().append("<div class='modal-body text-center mb-1'><h5 class='mt-1 mb-2 red-text'><i class='fa fa-warning'></i> Image can't be Uploaded!</h5><p class='mt-1 mb-2 light-blue-text'>Something went wrong in the server. Wait till the page refreshes...</p><div class='progress primary-color-dark'><div class='indeterminate'></div></div></div>").fadeIn("slow");        
-        },
-        complete: function(xhr) {
-          location.reload();
-        }
-      }); 
-    })();
-
-    $('.view_offer_details_button').click(function() {
-      var offer = $(this).data('offer');
-      var order = $(this).data('order');
-      $.ajaxSetup({
-        headers: {
-          'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr('content')
-        }
-      });
-      $.ajax({
-        url: "offer/details",
-        type: 'POST',
-        data: {
-          'offer_id': offer,
-          'order_id': order
-        },
-        dataType: 'JSON',
-        success:function(response){
-          $('#modal_offer_id').val(offer);
-          $('#modal_order_id').val(order);
-          $("#modal_accept_offer_btn").prop("disabled",false);
-          $("#modal_offer_details").empty().append('<ul class="list-group list-group-flush mb-3"><li class="list-group-item"><i class="fa fa-user fa-sm pr-2"></i><strong>Added By: </strong>' +response['user']+'</li><li class="list-group-item"><i class="fa fa-cart-plus fa-sm pr-2"></i><strong>Quantity: </strong>'+response['quantity']+'</li><li class="list-group-item"><i class="fa fa-dollar fa-sm pr-2"></i><strong>Asking Price: </strong>'+response['price']+'/=</li><li class="list-group-item"><i class="fa fa-calendar-check-o fa-sm pr-2"></i><strong>Delivery Date: </strong>'+response['date']+'</li></ul>'+response['details']);
-        },
-        error: function(response){
-          $("#modal_accept_offer_btn").prop("disabled",true);
-          $("#modal_offer_details").empty().append('<p class="font-weight-bold text-center">Sorry, no data returned from server...</p>');
-        }
-      });
-    })
-
-    $('.form_warning_sweet_alert').on('click',function(e){
-        e.preventDefault();
-        var form = $(this).parents('form');
-        var title = $(this).attr('title');
-        var text = $(this).attr('text');
-        var confirmButtonText = $(this).attr('confirmButtonText');
-        swal({
-            title: title,
-            text: text,
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#2196f3",
-            confirmButtonText: confirmButtonText,
-            closeOnConfirm: false
-        }, function(isConfirm){
-            if (isConfirm) form.submit();
-        });
-    });
-
-      // Jquery Datatable
-    $('#dataTable').DataTable();
-    $('#dataTable_wrapper').find('label').each(function () {
-      $(this).parent().append($(this).children());
-    });
-    $('#dataTable_wrapper .dataTables_filter').find('input').each(function () {
-      $('input').attr("placeholder", "Search");
-      $('input').removeClass('form-control-sm');
-    });
-    $('#dataTable_wrapper .dataTables_length').addClass('d-flex flex-row');
-    $('#dataTable_wrapper .dataTables_filter').addClass('md-form');
-    $('#dataTable_wrapper select').removeClass('custom-select custom-select-sm form-control form-control-sm');
-    $('#dataTable_wrapper select').addClass('mdb-select');
-    $('#dataTable_wrapper .mdb-select').material_select();
-    $('#dataTable_wrapper .dataTables_filter').find('label').remove();
-      // #ENDS# Jquery Datatable
 
 });
+
+/*==================== end of document ready functions  ====================*/
+
+/*==================== start of individual functions  ====================*/
+
+  // Function for aligning footer
 
 function footerAlign() {
   $('footer').css('display', 'block');
@@ -183,6 +206,8 @@ function footerAlign() {
   $('body').css('padding-bottom', footerHeight);
   $('footer').css('height', footerHeight);
 }
+
+  // Function for showing image preview
 
 function readURL(input, i) {
     i = i-1;
@@ -195,6 +220,8 @@ function readURL(input, i) {
     }
 }
 
+  // Function for printing a webpage area
+
 function printDiv(divName) {
      var printContents = document.getElementById(divName).innerHTML;
      var originalContents = document.body.innerHTML;
@@ -205,6 +232,8 @@ function printDiv(divName) {
 
      document.body.innerHTML = originalContents;
 }
+
+  // Function for tinymce editor
 
 $(function () {
     tinymce.init({
@@ -229,11 +258,15 @@ $(function () {
      });
 });
 
+  // Function for looping notificaitons
+
 function notificationLoop(time, data, redirect, colorName, placementFrom, placementAlign, offsetFrom, offsetAlign, animateEnter, animateExit){
   setTimeout(function () {   
     showNotification(data.find('h1').text(), data.find('p').text(), redirect, colorName, placementFrom, placementAlign, offsetFrom, offsetAlign, animateEnter, animateExit);                                       
   }, 1000 * time)
 }
+
+  // Function for showing notificaitons
 
 function showNotification(title, text, redirect, colorName, placementFrom, placementAlign, offsetFrom, offsetAlign, animateEnter, animateExit) {
     if (title === null || text === '') { text = ''; }
@@ -294,16 +327,4 @@ function showNotification(title, text, redirect, colorName, placementFrom, place
     });
 }
 
-
-
-
-
-/*$(document).ready(function() {
-
-    var docHeight = $(window).height();
-    var footerHeight = $('footer').outerHeight();
-    var footerTop = $('footer').position().top + footerHeight;
-
-    if (footerTop < docHeight)
-        $('footer').css('margin-top', 16+ (docHeight - footerTop) + 'px');
-});*/
+/*==================== end of individual functions  ====================*/
