ebc56074ccc4664a2e96aa4758cca83decfc1905
bappy25 <bappychanting@outlook.com>
2018-08-10 16:56:58 +0600

    added list

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -36,12 +36,39 @@
                     </a>
                     <!-- Messages Dropdown -->
                     <div class="dropdown-menu">
-                        <p class="text-center h6">Messages</p>
+                      <ul class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+                          <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">List group item heading</h5>
+                            <small>3 days ago</small>
+                          </div>
+                          <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                          <small>Donec id elit non mi porta.</small>
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                          <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">List group item heading</h5>
+                            <small class="text-muted">3 days ago</small>
+                          </div>
+                          <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                          <small class="text-muted">Donec id elit non mi porta.</small>
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                          <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">List group item heading</h5>
+                            <small class="text-muted">3 days ago</small>
+                          </div>
+                          <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                          <small class="text-muted">Donec id elit non mi porta.</small>
+                        </a>
+                      </ul>
+
+                        <!-- <p class="text-center h6">Messages</p>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Message 1</a>
                         <a class="dropdown-item" href="#">Message 2</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item text-center" href="user_messages.php">All Messages</a>
+                        <a class="dropdown-item text-center" href="user_messages.php">All Messages</a> -->
                     </div>
                     <!-- Messages Dropdown -->
                 </li>
